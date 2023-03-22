553ec2d11889b123f76ee48ff77c5880a503d9d1
anuj-9923 <anujkumar0799@gmail.com>
2023-03-23 02:17:35 +0530

    modified querySelector file

--- querySelector.js
+++ querySelector.js
@@ -1,10 +1,10 @@
-// var item2 = document.querySelector('.list-group-item:nth-child(2)');
-// console.log(item2);
-// item2.style.backgroundColor = 'green';
+var item2 = document.querySelector('.list-group-item:nth-child(2)');
+console.log(item2);
+item2.style.backgroundColor = 'green';
 
-// var item3 = document.querySelector('.list-group-item:nth-child(3)');
-// console.log(item3);
-// item3.style.display = 'none';
+var item3 = document.querySelector('.list-group-item:nth-child(3)');
+console.log(item3);
+item3.style.display = 'none';
 
 var items = document.querySelectorAll('.list-group-item');
 console.log(items);
