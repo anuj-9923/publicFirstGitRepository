7b3bb243892ff524b50b4fbffb3fb97d9e7ccdfc
anuj-9923 <anujkumar0799@gmail.com>
2023-03-26 12:40:35 +0530

    login form with Edit Button

--- formStoreData/loginFormWithNumber.js
+++ formStoreData/loginFormWithNumber.js
@@ -1,53 +1,70 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-console.log(itemList);
+var button = document.getElementById('info');
 //form submit Event
 form.addEventListener('submit', storeData);
 // Delete Functionality
 itemList.addEventListener('click', removeItem);
+
 //Store Data:
+var userDetail = {
+    name: '',
+    email: '',
+    phoneNumber: ''
+};
 function storeData(e) {
     e.preventDefault();
     // Get Input Value:
-    var name = document.getElementById('item').value;
-    var email = document.getElementById('item1').value;
-    var phoneNumber = document.getElementById('item2').value;
-    var userDetail = {
-        name: name,
-        email: email,
-        phoneNumber: phoneNumber
-    };
+    userDetail.name = document.getElementById('item').value;
+    userDetail.email = document.getElementById('item1').value;
+    userDetail.phoneNumber = document.getElementById('item2').value;
     var details = JSON.stringify(userDetail);
-    localStorage.setItem(email, details);
+    localStorage.setItem(userDetail.email, details);
     var li = document.createElement('li');
     //Add Class
     li.className = 'list-group';
 
     // Add text node with input value:
-    var list = '•' + name + '-' + email + '-' + phoneNumber;
-    console.log(list);
+    var list = '•' + userDetail.name + '-' + userDetail.email + '-' + userDetail.phoneNumber;
     li.appendChild(document.createTextNode(list));
     var deleteBtn = document.createElement('button');
     deleteBtn.className = 'delete';
     deleteBtn.style.width = '80px';
-
-
+    var editBtn = document.createElement('button');
+    editBtn.className = 'edit';
+    editBtn.style.width = '80px';
     // Append text node for button
     deleteBtn.appendChild(document.createTextNode('delete'));
+    //Append text node for edit button
+    editBtn.appendChild(document.createTextNode('edit'));
 
-    //appned Child
     li.appendChild(deleteBtn);
-
+    li.appendChild(editBtn);
+    editBtn.onclick = () => {
+        var it = document.querySelector('input');
+        localStorage.removeItem(userDetail.email);
+        console.log(it.parentElement);
+        document.getElementById('item').value = userDetail.name;
+        document.getElementById('item1').value = userDetail.email;
+        document.getElementById('item2').value = userDetail.phoneNumber;
+    }
     itemList.appendChild(li);
+
+
+
 }
 
 //Remove Item
 function removeItem(e) {
     if (e.target.classList.contains('delete')) {
-        console.log(1);
         if (confirm('Are you sure')) {
             var li = e.target.parentElement;
             itemList.removeChild(li);
         }
     }
+}
+function act() {
+    var input = document.getElementById('input').value;
+    input.value = "ANuj";
+    alert(input);
 }
